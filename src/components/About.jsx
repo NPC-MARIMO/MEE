67b79e406c6aaf3fb8cc7de16c4a7e7b27d827d3
Shivang Pandey <165462791+NPC-MARIMO@users.noreply.github.com>
2025-06-03 01:17:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,13 @@ function About() {
     <div className={styles.container} id="About">
       <h1 className={styles.title}>About</h1>
       <p className={styles.description}>
-      I’m a skilled Full-Stack Web & App Developer with expertise in JavaScript and frameworks like React.js, Next.js, and Three.js. I’m also a web designer
+     I’m a Full-Stack Web & App Developer with a focus on JavaScript, React.js, Next.js, and Three.js. Also a web designer with an eye for sleek UI.
       </p>
       <p style={{ marginTop: 30 }} className={styles.description}>
-        I’m a strong-willed person dedicated to my work and always looking for
-        the opportunities to learn and develop my skills and sharing knowledge.
+        I’m driven, always leveling up my skills, and love sharing knowledge along the way.
       </p>
       <p style={{ marginTop: 30 }} className={styles.description}>
-      I love coding, but outside of that, I enjoy playing tactical games like chess, sketching, and watching anime{" "}
+     When I’m not coding, you’ll find me deep into chess, sketching, or bingeing anime.
       </p>
     </div>
   );
